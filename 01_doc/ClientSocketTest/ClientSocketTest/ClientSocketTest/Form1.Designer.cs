9be4b17a2c94e3b848f6dab44b30e41e5cc5caa1
JinHee <30998424+KimJinHee921@users.noreply.github.com>
2020-02-20 15:09:31 +0900

    Add files via upload

---
+++ 01_doc/ClientSocketTest/ClientSocketTest/ClientSocketTest/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace ClientSocketTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.로그인요청ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.관제요청ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.정보변경요청ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.관제해제요청ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.재난종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.긴급영상요청ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.사회적약자ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.사건정보수신ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.재난ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(176, 131);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(576, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(176, 307);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(576, 136);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 490);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DimGray;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.사회적약자ToolStripMenuItem,
+            this.재난ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(910, 29);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.로그인요청ToolStripMenuItem,
+            this.관제요청ToolStripMenuItem,
+            this.정보변경요청ToolStripMenuItem,
+            this.관제해제요청ToolStripMenuItem,
+            this.재난종료ToolStripMenuItem});
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(49, 25);
+            this.toolStripMenuItem1.Text = "119";
+            // 
+            // 로그인요청ToolStripMenuItem
+            // 
+            this.로그인요청ToolStripMenuItem.Name = "로그인요청ToolStripMenuItem";
+            this.로그인요청ToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.로그인요청ToolStripMenuItem.Text = "로그인요청";
+            this.로그인요청ToolStripMenuItem.Click += new System.EventHandler(this.로그인요청ToolStripMenuItem_Click);
+            // 
+            // 관제요청ToolStripMenuItem
+            // 
+            this.관제요청ToolStripMenuItem.Name = "관제요청ToolStripMenuItem";
+            this.관제요청ToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.관제요청ToolStripMenuItem.Text = "관제요청";
+            this.관제요청ToolStripMenuItem.Click += new System.EventHandler(this.관제요청ToolStripMenuItem_Click);
+            // 
+            // 정보변경요청ToolStripMenuItem
+            // 
+            this.정보변경요청ToolStripMenuItem.Name = "정보변경요청ToolStripMenuItem";
+            this.정보변경요청ToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.정보변경요청ToolStripMenuItem.Text = "정보변경요청";
+            this.정보변경요청ToolStripMenuItem.Click += new System.EventHandler(this.정보변경요청ToolStripMenuItem_Click);
+            // 
+            // 관제해제요청ToolStripMenuItem
+            // 
+            this.관제해제요청ToolStripMenuItem.Name = "관제해제요청ToolStripMenuItem";
+            this.관제해제요청ToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.관제해제요청ToolStripMenuItem.Text = "관제해제요청";
+            this.관제해제요청ToolStripMenuItem.Click += new System.EventHandler(this.관제해제요청ToolStripMenuItem_Click);
+            // 
+            // 재난종료ToolStripMenuItem
+            // 
+            this.재난종료ToolStripMenuItem.Name = "재난종료ToolStripMenuItem";
+            this.재난종료ToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.재난종료ToolStripMenuItem.Text = "재난종료";
+            this.재난종료ToolStripMenuItem.Click += new System.EventHandler(this.재난종료ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.긴급영상요청ToolStripMenuItem});
+            this.toolStripMenuItem2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.toolStripMenuItem2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(49, 25);
+            this.toolStripMenuItem2.Text = "112";
+            // 
+            // 긴급영상요청ToolStripMenuItem
+            // 
+            this.긴급영상요청ToolStripMenuItem.Name = "긴급영상요청ToolStripMenuItem";
+            this.긴급영상요청ToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
+            this.긴급영상요청ToolStripMenuItem.Text = "112긴급영상요청";
+            this.긴급영상요청ToolStripMenuItem.Click += new System.EventHandler(this.긴급영상요청ToolStripMenuItem_Click);
+            // 
+            // 사회적약자ToolStripMenuItem
+            // 
+            this.사회적약자ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.사건정보수신ToolStripMenuItem});
+            this.사회적약자ToolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.사회적약자ToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.사회적약자ToolStripMenuItem.Name = "사회적약자ToolStripMenuItem";
+            this.사회적약자ToolStripMenuItem.Size = new System.Drawing.Size(108, 25);
+            this.사회적약자ToolStripMenuItem.Text = "사회적 약자";
+            // 
+            // 사건정보수신ToolStripMenuItem
+            // 
+            this.사건정보수신ToolStripMenuItem.Name = "사건정보수신ToolStripMenuItem";
+            this.사건정보수신ToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.사건정보수신ToolStripMenuItem.Text = "사건정보수신";
+            this.사건정보수신ToolStripMenuItem.Click += new System.EventHandler(this.사건정보수신ToolStripMenuItem_Click);
+            // 
+            // 재난ToolStripMenuItem
+            // 
+            this.재난ToolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.재난ToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.재난ToolStripMenuItem.Name = "재난ToolStripMenuItem";
+            this.재난ToolStripMenuItem.Size = new System.Drawing.Size(54, 25);
+            this.재난ToolStripMenuItem.Text = "재난";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(176, 173);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(576, 106);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.logInKeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(109, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "HEAD :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(109, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "DATA :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(103, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "요청값 :";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.title.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.title.Location = new System.Drawing.Point(54, 65);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(50, 25);
+            this.title.TabIndex = 11;
+            this.title.Text = "Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 563);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 로그인요청ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 관제요청ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 정보변경요청ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 관제해제요청ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 재난종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 긴급영상요청ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 사회적약자ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 사건정보수신ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 재난ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ 01_doc/ClientSocketTest/ClientSocketTest/ClientSocketTest/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace ClientSocketTest
+{
+    public partial class Form1 : Form
+    {
+        Socket client_socket;   //클라이언트 소켓 선언
+        bool isConnected;   //참-거짓 값을 이용하여 통신 오류 발생 시 대처
+        byte[] bytes = new byte[1024];
+        string data;
+        int dataLength;
+        string logInTime;
+        int statusBtn = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            isConnected = false;    //통신 시작되면 true
+            logInTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+            title.Text = "119/로그인요청";
+            statusBtn = 1;
+            HeadTextRefresh();
+            textBox3.Text = "로그인ID" + logInTime + "kor" + "|" + "화재" + "|" + "세부분류명" + "|" + "36.123456" + "|" + "126.12345488"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "법정동코드시도" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                start();
+                Thread listen_thread = new Thread(do_receive);
+                listen_thread.Start();  //클라이언트 스레드 시작(서버 시작)
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+
+            try
+            {
+                if (isConnected == false) return;
+                byte[] msg = Encoding.UTF8.GetBytes(textBox1.Text + textBox3.Text + ";");    //UTF8로 인코딩 상대에게 OP : 데이터 + "<eof>" 송신
+                int bytesSent = client_socket.Send(msg);    //Send를 이용해서 내용 송신
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+		//	listBox1.SelectedIndex = listBox1.Items.Count - 1;
+		}
+
+        public void start()
+        {
+            try
+            {
+                if (isConnected == true) return;
+                client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);    //소켓 생성
+                client_socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+                listBox1.Items.Add(String.Format("소켓 연결되었습니다 ", client_socket.RemoteEndPoint.ToString()));
+                isConnected = true;
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+              //  listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+        }
+
+        void do_receive()
+        {
+            while (isConnected) //무한루프 설정
+            {
+                try
+                {
+                    while (true)    //통신이 시작되었을때
+                    {
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = client_socket.Receive(bytes);
+                        data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf(";") > -1) break;  //<eof>가 있으면 브레이크
+                    }
+                    CheckForIllegalCrossThreadCalls = false;
+                    Invoke((MethodInvoker)delegate  // ? 없으면 크로스스레드 오류 발생 (https://blog.naver.com/spb02293/221671835503 참조)
+                        {
+                            listBox1.Items.Add(data);
+                            listBox1.Items.Add("데이터 수신 완료");
+                        }
+                    );
+                    data = "";
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                    MessageBox.Show(ex.ToString());
+
+                    isConnected = false;
+                    while (isConnected == false)
+                    {
+                        start();
+                        Thread listen_thread = new Thread(do_receive);
+                        listen_thread.Start();
+                    }
+                }
+                finally
+                {
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                }
+            }
+        }
+
+        private void 로그인요청ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 1;
+            textBox1.Clear();
+            title.Text = "119/로그인요청";
+            HeadTextRefresh();
+            textBox3.Text = "로그인ID" + logInTime + "kor" + "|" + "화재" + "|" + "세부분류명" + "|" + "36.123456" + "|" + "126.12345488"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "법정동코드시도" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+        private void 관제요청ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 2;
+            textBox1.Clear();
+            title.Text = "119/관제요청";
+            HeadTextRefresh();
+            textBox3.Text = "긴급구조표준접수번호" + "|" + "화재" + "|" + "세부분류명" + "|" + "36.123456" + "|" + "126.12345488"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "법정동코드시도" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+
+        private void 정보변경요청ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 3;
+            textBox1.Clear();
+            title.Text = "119/정보변경요청";
+            HeadTextRefresh();
+            textBox3.Text = "긴급구조표준접수번호" + "|" + "화재" + "|" + "세부분류명" + "|" + "36.123456" + "|" + "126.12345488"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "법정동코드시도" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+        private void 관제해제요청ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 4;
+            textBox1.Clear();
+            title.Text = "119/관제해제요청";
+            HeadTextRefresh();
+            textBox3.Text = "긴급구조표준접수번호" + "|" + "화재" + "|" + "세부분류명" + "|" + "36.123456" + "|" + "126.12345488"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "법정동코드시도" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+        private void 재난종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 5;
+            textBox1.Clear();
+            title.Text = "119/재난종료";
+            HeadTextRefresh();
+            textBox3.Text = "긴급구조표준접수번호" + "|" + "화재" + "|" + "세부분류명" + "|" + "36.123456" + "|" + "126.12345488"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "법정동코드시도" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+        private void 긴급영상요청ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 6;
+            textBox1.Clear();
+            title.Text = "112/112긴급영상요청";
+            HeadTextRefresh();
+            textBox3.Text = "사건번호" + "|" + "사고등급" + "|" + "사고유형" + "|" + "사건번호" + "|" + "126.12345488" + "|" + "36.123456"
+                            + "|" + "서울 금천구 가산동 17-1 (공공건물)" + "|" + "지역코드" + "|" + logInTime + "|" + "사용자ID" + ";";
+        }
+
+        private void 사건정보수신ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            statusBtn = 7;
+            textBox1.Clear();
+            title.Text = "사회적약자/사건정보수신";
+            HeadTextRefresh();
+            textBox3.Text = "발생번호" + "|" + "서비스유형" + "|" + "126.12345488" + "|" + "36.123456"
+                            + "|" + "사건지점전체주소" + "|" + "지역코드" + "|" + "Ref_ID" + "|" + "이름"
+                            + "|" + "핸드폰번호" + "|" + "출생일" + "|" + "성별" + "|" + "주소(주거지)"
+                            + "|" + "보호자이름" + "|" + "보호자 핸드폰" + "|" + logInTime + "|" + "이미지파일"
+                            + "|" + "신상정보" + "|" + "특이사항" + "|" + "전송자ID" + "|" + "주소(주거지)"
+                            + ";";
+        }
+
+        private void logInKeyPress(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            dataLength = textBox3.Text.Length;
+            HeadTextRefresh();
+        }
+
+        public void HeadTextRefresh()
+        {
+            dataLength = textBox3.Text.Length;
+            var currentTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+            var dataFormat = string.Format("{0:D10}", dataLength);
+            var code1 = "";
+            var code2 = "";
+            var code3 = "";
+            if (statusBtn == 1)
+            {
+                code1 = "99";
+                code2 = "99";
+                code3 = "119";
+            }
+            else if (statusBtn == 2)
+            {
+                code1 = "10";
+                code2 = "10";
+                code3 = "119";
+            }
+            else if (statusBtn == 3)
+            {
+                code1 = "10";
+                code2 = "40";
+                code3 = "119";
+            }
+            else if (statusBtn == 4)
+            {
+                code1 = "10";
+                code2 = "50";
+                code3 = "119";
+            }
+            else if (statusBtn == 5)
+            {
+                code1 = "10";
+                code2 = "91";
+                code3 = "119";
+            }
+            else if (statusBtn == 6)
+            {
+                code1 = "10";
+                code2 = "10";
+                code3 = "112";
+            }
+            else if (statusBtn == 7)
+            {
+                code1 = "10";
+                code2 = "10";
+                code3 = "WP1";
+            }
+        
+            string[] stringArray = { code1, code2, currentTime, code3, "UCP", currentTime, dataFormat }; 
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                textBox1.Text += stringArray[i];
+            }
+        }
+    }
+}
